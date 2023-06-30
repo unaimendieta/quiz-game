a41f8a99f70f10dd709a41b28c2982f07b4c609d
unaimendieta <unaimendieta@gmail.com>
2023-06-30 12:47:44 +0200

    Update Timer.jsx

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Timer = ({id,time, action, counter}) => {
     const [timer, setTimer] = useState(time);
-    console.log("render");
+
     useEffect(()=>{
         if (timer===0){
             setTimer(time);
@@ -12,9 +12,11 @@ const Timer = ({id,time, action, counter}) => {
         const timerTimeout = setTimeout(() => setTimer(timer-1), 1000);
         return ()=> clearTimeout(timerTimeout);
     },[timer]);
+
     useEffect(()=>{
             setTimer(time);
     },[counter]);
+    
 	return (
 	<>
         <h2 id={id}>{timer}</h2>
